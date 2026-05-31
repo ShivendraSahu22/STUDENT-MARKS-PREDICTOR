38cd717a745d53981a6e19c1084251ee77eeec13
ShivendraSahu22 <shivendrasahu518@gmail.com>
2026-05-31 15:39:05 +0530

    schemawith app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-#from schema.user_input import UserInput
+from schema.user_input import UserInput
 from schema.prediction_response import PredictionResponse
 #from model.predict import predict_output, model, MODEL_VERSION
 
