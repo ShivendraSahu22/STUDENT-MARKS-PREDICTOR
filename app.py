008fcb317332df306e22ba0d5f54f12964c86225
ShivendraSahu22 <shivendrasahu518@gmail.com>
2026-05-31 15:28:38 +0530

    working

---
+++ app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from schema.user_input import UserInput
+from schema.prediction_response import PredictionResponse
+from model.predict import predict_output, model, MODEL_VERSION
+
+app = FastAPI()
+
+# human readable       
+@app.get('/')
+def home():
+    return {'message':'Insurance Premium Prediction API'}
