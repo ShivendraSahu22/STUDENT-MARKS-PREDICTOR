b708c6455f4f4fa3119671551dffdd8a16d3e4fa
ShivendraSahu22 <shivendrasahu518@gmail.com>
2026-06-05 21:29:41 +0530

    taking user input

--- model/predict.py
+++ model/predict.py
@@ -5,3 +5,12 @@ with open('model/model.pkl', 'rb') as f:
 
 # MLFlow
 MODEL_VERSION = '1.0.0'
+
+def predict_marks(user_input):
+    # Convert user input to a format suitable for prediction
+    input_data = [[user_input.number_courses, user_input.time_study]]
+    
+    # Make prediction using the loaded model
+    predicted_marks = model.predict(input_data)
+    
+    return predicted_marks[0]
