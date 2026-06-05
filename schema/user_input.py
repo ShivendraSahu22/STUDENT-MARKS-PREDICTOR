a70b2a0d0876ceac2cef8133af846b731b99116f
ShivendraSahu22 <shivendrasahu518@gmail.com>
2026-06-05 21:40:09 +0530

    marks import

--- schema/user_input.py
+++ schema/user_input.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field, field_validator
 from typing import Annotated
 from schema.user_input import UserInput
-from model.predict import predict_output, model, MODEL_VERSION
+from model.predict import predict_marks, model, MODEL_VERSION
 
 
 
