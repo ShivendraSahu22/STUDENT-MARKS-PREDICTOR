06345e9a00bd3fab03be92f138a719f7b9de84fd
ShivendraSahu22 <shivendrasahu518@gmail.com>
2026-06-05 22:18:15 +0530

    dockerfile

--- schema/prediction_response.py
+++ schema/prediction_response.py
@@ -3,5 +3,6 @@ from pydantic import BaseModel, Field
 class PredictionResponse(BaseModel):
     predicted_marks: float = Field(
         ...,
-        description="The predicted student marks"
+        description="The predicted student marks",
+        example=85.5
     )
