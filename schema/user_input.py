2a5d190eabb8203e382881620d6dc6b7c40bcfb0
ShivendraSahu22 <shivendrasahu518@gmail.com>
2026-06-05 21:45:45 +0530

    fix

--- schema/user_input.py
+++ schema/user_input.py
@@ -4,8 +4,8 @@ from typing import Annotated
 
 class UserInput(BaseModel):
 
-     'number_courses': Annotated[int, Field(..., description="Number of courses taken by the student")]
-     'time_study': Annotated[float, Field(..., description="Time spent studying")]
+     number_courses: Annotated[int, Field(..., description="Number of courses taken by the student")]
+     time_study: Annotated[float, Field(..., description="Time spent studying")]
 
      @field_validator('number_courses')
      def validate_number_courses(cls, value):
